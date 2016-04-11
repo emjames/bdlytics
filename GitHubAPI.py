e6ed478cab14f94405215e334ddb3f2ec33bfd19
james0x41 <james0x41@gmail.com>
2016-04-11 23:25:25 +0800

    Adds GithubException to __init__

--- GitHubAPI.py
+++ GitHubAPI.py
@@ -5,20 +5,27 @@
 #
 
 import json
-from github import Github
+from github import Github, GithubException
 from pprint import pprint as pp
 import requests
 
 
 class GithubAPI(object):
-    def __init__(self, user='emjames'):
+    def __init__(self):
         with open('keys.json') as keys_file:
             keys = json.load(keys_file)
 
         # get access_token from external file
         self.access_token = keys["github"]["access_token"]
-        self.g = Github(self.access_token, per_page=100)
-        self.user = self.g.get_user(user)
+        # self.g = Github(self.access_token, per_page=100)
+        self.g = Github(self.access_token)
+        try:
+            self.g.get_user().login
+        # test the token
+        except GithubException as e:
+            print e.status + e.message
+
+        self.user = self.g.get_user()
         self.repo = None
         # self.repo = self.user.get_repo(repo)
 
@@ -46,3 +53,10 @@ class GithubAPI(object):
         return [(repo['name'],
                  repo['url'])
                 for repo in resp]
+
+    # get issues from a repo
+    def getIssues(self):
+        q = 'label:bug+language:python&sort=created&order=asc'
+        # resp = requests.get(Github.search_repositories(query=q))
+        resp = self.g.search_issues(query=q)
+        pp(resp)
